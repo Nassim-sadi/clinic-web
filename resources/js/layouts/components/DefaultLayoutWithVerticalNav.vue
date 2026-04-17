8bf87c7dd62b9bdfbf70af01e81ccf54b130e4ab
nassim-sadi <nassim.sadi@hotmail.com>
2026-04-17 03:32:07 +0100

    Use original customizer with fixed side button, remove extra toggle

--- resources/js/layouts/components/DefaultLayoutWithVerticalNav.vue
+++ resources/js/layouts/components/DefaultLayoutWithVerticalNav.vue
@@ -8,7 +8,6 @@ import NavBarNotifications from '@/layouts/components/NavBarNotifications.vue'
 import NavSearchBar from '@/layouts/components/NavSearchBar.vue'
 import NavbarShortcuts from '@/layouts/components/NavbarShortcuts.vue'
 import NavbarThemeSwitcher from '@/layouts/components/NavbarThemeSwitcher.vue'
-import NavbarCustomizerToggle from '@/layouts/components/NavbarCustomizerToggle.vue'
 import UserProfile from '@/layouts/components/UserProfile.vue'
 import NavBarI18n from '@core/components/I18n.vue'
 
@@ -41,7 +40,6 @@ import { VerticalNavLayout } from '@layouts'
           :languages="themeConfig.app.i18n.langConfig"
         />
         <NavbarThemeSwitcher />
-        <NavbarCustomizerToggle />
         <NavbarShortcuts />
         <NavBarNotifications class="me-1" />
         <UserProfile />
@@ -55,5 +53,8 @@ import { VerticalNavLayout } from '@layouts'
     <template #footer>
       <Footer />
     </template>
+
+    <!-- 👉 Customizer -->
+    <TheCustomizer />
   </VerticalNavLayout>
 </template>
